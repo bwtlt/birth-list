886ab3bbe9a9fdc3e183eac1626a57aec43db083
bwtlt <bertrandwatelet@gmail.com>
2022-09-25 16:37:21 +0200

    added filters for category and priority

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,13 @@ import styles from '../styles/BirthList.module.scss'
 import { useAuth } from '../context/AuthUserContext';
 
 const db = firebase.firestore();
+const CATEGORIES = ["Les repas", "Les sorties", "La chambre", "La toilette", "L'éveil", "Les souvenirs"];
+const PRIORITIES = ["« Prioritaire », nécessaire avant la naissance.", "« Les petits essentielles », peut attendre après la naissance."];
 
 export default function Home() {
   const [itemsArray, setItemsArray] = useState([]);
-  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("Tout");
+  const [priority, setPriority] = useState(-1);
   const { authUser, loading, signInWithEmailAndPassword } = useAuth();
   const email = process.env.NEXT_PUBLIC_FIREBASE_FIRESTORE_EMAIL;
   const [password, setPassword] = useState("");
@@ -22,10 +25,6 @@ export default function Home() {
         querySnapshot.forEach((doc) => {
           const item = doc.data();
           item.id = doc.id;
-          const itemCategory = item.category
-          if (categories.indexOf(itemCategory) === -1) {
-            setCategories(categories => [...categories, itemCategory])
-          }
           setItemsArray(itemsArray => [...itemsArray, item]);
         });
       });
@@ -70,21 +69,47 @@ export default function Home() {
                 </form>
               </div>
             }
-            {authUser && <><div className={styles.intro}>
-              <div className={styles.introText}>
-                <p>Cela fait six mois que nous nous préparons à l’arrivée de notre petite fille. Vous vous projetez avec nous dans ce nouveau chapitre de notre vie et cela nous touche profondément. Vous nous demandez souvent ce qui nous ferait plaisir, ce dont nous aurions besoin ou comment nous aider à accueillir au mieux notre petit bout de chou dans notre maison roulante.</p>
-                <p>Alors avec Bertrand nous avons listé nos envies ci-dessous, et on aimerait beaucoup que vous privilégiez dans la mesure du possible des produits de seconde main ou fait main : qu’ils proviennent de votre garage, de Vinted, du bon coin, de créateurs et autres ça nous ferait très plaisir.</p>
-                <p>Pour les achats chez Autour De Bébé, nous avons une carte fidélité au nom de Laëtitia Watelet.</p>
-              </div>
-              <Image className={styles.introImage} src="/parents.jpg" height={500} width={500} alt="Les heureux parents"></Image>
-            </div>
-              <div className={styles.list}>
-                <div className={styles.listTitle}>Liste prioritaire</div>
-                {itemsArray.map((item) => {
-                  return (<Item key={item.id} item={item} database={db} />)
-                })}
-              </div>
-            </>
+            {authUser &&
+              <>
+                <div className={styles.intro}>
+                  <div className={styles.introText}>
+                    <p>Cela fait six mois que nous nous préparons à l’arrivée de notre petite fille. Vous vous projetez avec nous dans ce nouveau chapitre de notre vie et cela nous touche profondément. Vous nous demandez souvent ce qui nous ferait plaisir, ce dont nous aurions besoin ou comment nous aider à accueillir au mieux notre petit bout de chou dans notre maison roulante.</p>
+                    <p>Alors avec Bertrand nous avons listé nos envies ci-dessous, et on aimerait beaucoup que vous privilégiez dans la mesure du possible des produits de seconde main ou fait main : qu’ils proviennent de votre garage, de Vinted, du bon coin, de créateurs et autres ça nous ferait très plaisir.</p>
+                    <p>Pour les achats chez Autour De Bébé, nous avons une carte fidélité au nom de Laëtitia Watelet.</p>
+                  </div>
+                  <Image className={styles.introImage} src="/parents.jpg" height={500} width={500} alt="Les heureux parents"></Image>
+                </div>
+                <form action="#" className={styles.filterControls}>
+                  <select name="category" id="category" onChange={(e) => {
+                    setCategory(e.target.value);
+                  }}>
+                    <option value="Tout">Sélectionner une catégorie...</option>
+                    {CATEGORIES.map((cat) => {
+                      return(<option value={cat} key={cat}>{cat}</option>)
+                      })}
+                  </select>
+                  <select name="priority" id="priority" onChange={(e) => {
+                    setPriority(e.target.value);
+                  }}>
+                    <option value={-1}>Sélectionner une priorité...</option>
+                    <option value={0}>Prioritaire</option>
+                    <option value={1}>Secondaire</option>
+                  </select>
+                </form>
+
+                {priority >= 0 && <div className={styles.listTitle}>Liste {PRIORITIES[priority]}</div>}
+                <div className={styles.sublistTitle}>Categorie : {category}</div>
+
+                <div className={styles.list}>
+                  {itemsArray.filter((item) => {
+                    const hasPriority = priority == -1 || item.priority == priority;
+                    const hasCategory = category == "Tout" || item.category == category;
+                    return hasPriority && hasCategory;
+                  }).map((item) => {
+                    return (<Item key={item.id} item={item} database={db} />)
+                  })}
+                </div>
+              </>
             }
           </div>
         </div>
