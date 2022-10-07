5b32f8b6bc0f5a3f5a14a426fb7331ce7b3d2af0
bwtlt <bertrandwatelet@gmail.com>
2022-10-07 22:38:36 +0200

    add under maintenance page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import { AuthUserProvider } from '../context/AuthUserContext';
+import Maintenance from './maintenance';
 
 function MyApp({ Component, pageProps }) {
-  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
+  return <AuthUserProvider>
+    {process.env.NEXT_PUBLIC_MAINTENANCE_MODE ? <Maintenance/> : <Component {...pageProps} />}
+  </AuthUserProvider>
 }
 
 export default MyApp
---
+++ pages/maintenance.js
@@ -0,0 +1,23 @@
+import styles from '../styles/Maintenance.module.scss'
+import Image from 'next/image';
+
+export default function Maintenance() {
+  return (
+    <main>
+      <div className={styles.body}>
+        <div id="article">
+          <div className={styles.title}>La liste de naissance revient vite !</div>
+          <div className={styles.content}>
+          <div className={styles.introImageContainer}>
+            <Image className={styles.introImage} src="/maintenance.jpg" layout="fill" objectFit="contain" alt="Les parents perdus"></Image>
+          </div>
+          <div>
+            <p>Nous sommes désolés pour le désagrément, nous sommes en train d&apos;effectuer une maintenance.</p>
+            <p>— Laëtitia et Bertrand</p>
+          </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
